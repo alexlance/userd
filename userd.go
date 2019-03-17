5249a57aa31a33b51ff3feac7ccff95d69878f58
Alex Lance <code@alexlance.com>
2019-03-18 01:58:08 +1100

    Bump version number

--- userd.go
+++ userd.go
@@ -51,7 +51,7 @@ var (
 )
 
 func init() {
-	log.SetPrefix("userd v1.14 ")
+	log.SetPrefix("userd v1.15 ")
 	if os.Geteuid() != 0 {
 		log.Fatalf("Error: Bad user id (%d), must run as root", os.Geteuid())
 	}
