9e07b7dbdd9e9b31673004705e253eab630d50f6
Alex Lance <code@alexlance.com>
2019-09-27 12:07:53 +1000

    Ensure version gets bumped in README.md for new release

--- version.sh
+++ version.sh
@@ -9,7 +9,8 @@ gh="https://api.github.com/repos/${user}/${repo}"
 c="curl -sfL -u ${user}:${token}"
 currentversion=$(${c} ${gh}/releases | jq -r '.[0].name' | tr -d v)
 version="v$(echo "${currentversion} + 0.01" | bc)"
-grep "userd ${version}" main.go || (echo "Wrong version in main.go" && exit 1)
+grep "userd ${version}" main.go   || (echo "Wrong version in main.go"   && exit 1)
+grep "userd ${version}" README.md || (echo "Wrong version in README.md" && exit 1)
 git tag ${version}
 git push origin master
 release=$(${c} -XPOST ${gh}/releases -d '{ "tag_name": "'${version}'", "target_commitish": "master", "name": "'${version}'", "body": "'${version}'", "draft": false, "prerelease": false }')
